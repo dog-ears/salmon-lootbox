f27bb66dbf8eacb5c26a88f8133e3b887fe51d0c
dog-ears <info@dog-ears.net>
2020-09-01 08:43:21 +0900

    不要ファイルの削除と基本scssの導入

---
+++ src/component/app/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// スタイル
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>appのh1</h1>
+      <h2>h2テスト</h2>
+      <ul>
+        <li>liテスト</li>
+        <li>liテスト</li>
+        <li>liテスト</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// スタイル
+import './index.scss';
+
+// コンポーネント
+import App from 'component/app/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>indexのh1</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
