ebc725192f9c9042c282b9651f7d3f24d5ad4e84
dog-ears <info@dog-ears.net>
2020-09-03 06:56:20 +0900

    RootStateの設定

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -3,18 +3,33 @@ import React from 'react';
 // スタイル
 import './App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <h1>appのh1</h1>
-      <h2>h2テスト</h2>
-      <ul>
-        <li>liテスト</li>
-        <li>liテスト</li>
-        <li>liテスト</li>
-      </ul>
-    </div>
-  );
+interface StateInterface {
+  weaponInventory: WeaponInventoryInterface[],
+}
+interface WeaponInventoryInterface {
+  weaponId: number,
+  amount: number,
 }
 
-export default App;
+export default class App extends React.Component<{}, StateInterface> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      weaponInventory: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>appのh1</h1>
+        <h2>h2テスト</h2>
+        <ul>
+          <li>liテスト</li>
+          <li>liテスト</li>
+          <li>liテスト</li>
+        </ul>
+      </div>
+    );
+  }
+}
