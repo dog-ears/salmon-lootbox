eee092f1bc491d89902a78dd51dc814fb28e4aa7
dog-ears <info@dog-ears.net>
2020-09-05 11:03:35 +0900

    weaponListのスタイルを調整

--- src/component/weaponList/WeaponList.tsx
+++ src/component/weaponList/WeaponList.tsx
@@ -12,23 +12,29 @@ export default class WeaponList extends React.Component<PropsInterface, {}> {
 
   render() {
     return (
-      <div className="weaponList">
-        <div className="container">
-          <h2>ブキリスト</h2>
-          <div className="myWeapon">
-            <ul>
-              {this.props.weaponInventory.map((wi: WeaponInventoryInterface) => {
-                return (
-                  <li
-                    key={wi.weaponId}
-                    className={Weapons.getById(wi.weaponId).filename + ((wi.amount === 0) ? ' notOwn' : '')}
-                  >
-                    <div className='name'>{Weapons.getById(wi.weaponId).name}（{wi.amount}）</div>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+      <div id="weaponList" className="container">
+        <h2>ブキリスト</h2>
+        <div className="myWeapon">
+          <ul>
+            {this.props.weaponInventory.map((wi: WeaponInventoryInterface) => {
+              return (
+                <li
+                  key={wi.weaponId}
+                  className={Weapons.getById(wi.weaponId).filename + ((wi.amount === 0) ? ' notOwn' : '')}
+                >
+                  <div className='name'>{Weapons.getById(wi.weaponId).name}（{wi.amount}）</div>
+                </li>
+              )
+            })}
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+            <li className="blank"></li>
+          </ul>
         </div>
       </div>
     );
